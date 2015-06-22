28d7f64e6f72dd107f442765fd5cb8bee8f5584f
qiugui <qiugui0557@live.com>
2015-06-22 11:24:37 +0800

    2015/06/22

--- test/com/lucene/utils/test/TestIndexUtil.java
+++ test/com/lucene/utils/test/TestIndexUtil.java
@@ -1,26 +1,66 @@
  package com.lucene.utils.test;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.lucene.utils.IndexUtil;
  public class TestIndexUtil {
 
+	 IndexUtil iu = null;
+	 
+	 @Before
+	 public void beforeTest() {
+		 if (iu == null) {
+			 iu = new IndexUtil();
+		 }
+	 }
+	 
 	 @Test
 	 public void testIndex() {
-		 IndexUtil iu = new IndexUtil();
 		 iu.index();
 	 }
 	 
 	 @Test
 	 public void testQuery() {
-		 IndexUtil iu = new IndexUtil();
 		 iu.query();
 	 }
 	 
 	 @Test
 	 public void testDelete() {
-		 IndexUtil iu = new IndexUtil();
 		 iu.delete();
+	 }
+	 
+	 @Test
+	 public void testUnDelete() {
+		iu.undelete();
+	}
+	 
+	 @Test
+	 public void testForceMergeDelete() {
+		iu.forceMergeDeletes();
+	}
+	 
+	 @Test
+	 public void testMerge() {
+		iu.merge();
+	}
+	 
+	 @Test
+	 public void testUpdate() {
+		iu.update();
+	}
+	 
+	 @Test
+	 public void testSearch() {
+		for (int i=0;i<5;i++){
+			iu.search();
+			System.out.println();
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				 e.printStackTrace();
+			}
+		}
 	 }
 }
 
